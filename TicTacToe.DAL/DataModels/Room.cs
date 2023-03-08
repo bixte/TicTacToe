d5ed1a4419a4b14de282de4e1f5c3fb790345066
artem <artm.isWorking@gmail.com>
2023-03-08 06:38:03 +0300

    change on Room.cs. since multiple  cascade paths, was solved to do foreign keys may be null.

--- TicTacToe.DAL/DataModels/Room.cs
+++ TicTacToe.DAL/DataModels/Room.cs
@@ -1,12 +1,14 @@
-﻿namespace TicTacToe.DataModels.DAL
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicTacToe.DataModels.DAL
 {
     public class Room
     {
         public int Id { get; set; }
         public bool IsOver { get; set; } = false;
-        public Player PlayerX { get; set; } = null!;
-        public Player Player0 { get; set; } = null!;
-        public int? PlayerWin { get; set; }
+        public Player? PlayerX { get; set; } = null!;
+        public Player? Player0 { get; set; } = null!;
+        public char? PlayerWin { get; set; }
         public List<Step>? Steps { get; set; }
     }
 }
