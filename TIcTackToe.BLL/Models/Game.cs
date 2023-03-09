3cf4fc07ad208badd19679eedb32c8952cd30896
artem <artm.isWorking@gmail.com>
2023-03-09 12:41:52 +0300

    Add new method to game and change args

--- TIcTackToe.BLL/Models/Game.cs
+++ TIcTackToe.BLL/Models/Game.cs
@@ -11,7 +11,7 @@ namespace TicTacToe.Models
             this.room = room;
             Fields = GetFields();
         }
-        public bool CanMove(byte row, byte col)
+        public bool CanMove(int row, int col)
         {
             if (!room.IsOver)
             {
@@ -23,22 +23,23 @@ namespace TicTacToe.Models
 
         public char CurrentValue()
         {
-            if (room.Steps?.Count % 2 != 0)
+            if ((room.Steps?.Count + 1) % 2 == 0)
+                return '0';
+            else
                 return 'x';
-            return '0';
         }
         public bool IsWin()
         {
-            if (Fields[0, 0] == Fields[0, 1] && Fields[0, 0] == Fields[0, 2] ||
-                Fields[1, 0] == Fields[1, 1] && Fields[1, 0] == Fields[1, 2] ||
-                Fields[2, 0] == Fields[2, 1] && Fields[2, 0] == Fields[2, 2] ||
+            if (Fields[0, 0].HasValue && Fields[0, 0] == Fields[0, 1] && Fields[0, 0] == Fields[0, 2] ||
+                Fields[1, 0].HasValue && Fields[1, 0] == Fields[1, 1] && Fields[1, 0] == Fields[1, 2] ||
+                Fields[2, 0].HasValue && Fields[2, 0] == Fields[2, 1] && Fields[2, 0] == Fields[2, 2] ||
                 //check horizontal
-                Fields[0, 0] == Fields[1, 0] && Fields[0, 0] == Fields[2, 0] ||
-                Fields[0, 1] == Fields[1, 1] && Fields[0, 1] == Fields[2, 2] ||
-                Fields[0, 2] == Fields[1, 2] && Fields[0, 2] == Fields[2, 2] ||
+                Fields[0, 0].HasValue && Fields[0, 0] == Fields[1, 0] && Fields[0, 0] == Fields[2, 0] ||
+                Fields[0, 1].HasValue && Fields[0, 1] == Fields[1, 1] && Fields[0, 1] == Fields[2, 2] ||
+                Fields[0, 2].HasValue && Fields[0, 2] == Fields[1, 2] && Fields[0, 2] == Fields[2, 2] ||
                 //check vertical
-                Fields[0, 0] == Fields[1, 1] && Fields[0, 0] == Fields[2, 2] ||
-                Fields[0, 2] == Fields[1, 1] && Fields[0, 2] == Fields[2, 0]
+                Fields[0, 0].HasValue && Fields[0, 0] == Fields[1, 1] && Fields[0, 0] == Fields[2, 2] ||
+                Fields[0, 2].HasValue && Fields[0, 2] == Fields[1, 1] && Fields[0, 2] == Fields[2, 0]
                 //check cross
                 )
             {
@@ -46,6 +47,7 @@ namespace TicTacToe.Models
             }
             return false;
         }
+        public void AddStep(int row, int col, char value) => Fields[row, col] = value;
         private char?[,] GetFields()
         {
             var fields = new char?[3, 3];
--- TicTacToe.DAL/DataModels/Step.cs
+++ TicTacToe.DAL/DataModels/Step.cs
@@ -3,8 +3,8 @@
     public class Step
     {
         public int Id { get; set; }
-        public byte Row { get; set; }
-        public byte Col { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
         public char Value { get; set; }
     }
 }
