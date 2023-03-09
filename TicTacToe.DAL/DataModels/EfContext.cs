fde75ab7acde4a1c2faa012737e0d10d6af9b422
artem <artm.isWorking@gmail.com>
2023-03-09 09:51:12 +0300

    add autoincude

--- TicTacToe.DAL/DataModels/EfContext.cs
+++ TicTacToe.DAL/DataModels/EfContext.cs
@@ -10,5 +10,12 @@ namespace TicTacToe.DataModels.DAL
         }
         public DbSet<Player> Players { get; set; } = null!;
         public DbSet<Room> Rooms { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>().Navigation(r => r.PlayerX).AutoInclude();
+            modelBuilder.Entity<Room>().Navigation(r => r.Player0).AutoInclude();
+            modelBuilder.Entity<Room>().Navigation(r => r.PlayerWin).AutoInclude();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
